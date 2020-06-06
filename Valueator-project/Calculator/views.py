0c8486e209c3e0209270ecbcd659e09ecb9de25b
ken-10 <46728332+ken-10@users.noreply.github.com>
2020-06-05 23:15:08 -1000

    Displays income and expenditure objects

--- Valueator-project/Calculator/views.py
+++ Valueator-project/Calculator/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Income, Expenditure
 
+#Get income and expenditure and display them
 def calculator(request):
-    return render(request, "Calculator/calculator.html")
+    latest_income_list = Income.objects.get(user = request.user)
+    latest_expenditure_list = Expenditure.objects.filter(user = request.user)
+    context = {'latest_income': latest_income_list, 'latest_expenditure': latest_expenditure_list}
+    print(context)
+    return render(request, "Calculator/calculator.html", context)
