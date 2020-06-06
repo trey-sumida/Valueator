4c2f7fe2d0a943af05a3cbdf96a43aa11d998965
ken-10 <46728332+ken-10@users.noreply.github.com>
2020-06-06 00:13:39 -1000

    User can add new income, new expenses, and update income

--- Valueator-project/Calculator/urls.py
+++ Valueator-project/Calculator/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'Calculator'
 urlpatterns = [
     path('', views.calculator, name='calculator'),
+    path('add_expenditure/', views.addExpenditure, name='addExpenditure'),
+    path('add_income/', views.addIncome, name='addIncome')
 ]
--- Valueator-project/Calculator/views.py
+++ Valueator-project/Calculator/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Income, Expenditure
 
 #Get income and expenditure and display them
 def calculator(request):
-    latest_income_list = Income.objects.get(user = request.user)
-    latest_expenditure_list = Expenditure.objects.filter(user = request.user)
-    context = {'latest_income': latest_income_list, 'latest_expenditure': latest_expenditure_list}
-    print(context)
-    return render(request, "Calculator/calculator.html", context)
+    try:
+        latest_income_list = Income.objects.get(user = request.user)
+        latest_expenditure_list = Expenditure.objects.filter(user = request.user)
+        context = {'latest_income': latest_income_list, 'latest_expenditure': latest_expenditure_list}
+        print(context)
+        return render(request, "Calculator/calculator.html", context)
+    except:
+        return render(request, "Calculator/calculator.html")
+
+def addIncome(request):
+    if request.method == 'POST':
+        user_income_input = request.POST["income_text"]
+        try:
+            prev_income = Income.objects.get(user = request.user)
+            prev_income.income_text = user_income_input
+            prev_income.save()
+        except:
+            input = Income.objects.create(income_text = user_income_input, user = request.user)
+        return redirect("Calculator:calculator")
+
+def addExpenditure(request):
+    if request.method == 'POST':
+        user_expenditure = request.POST["expenditure_text"]
+        user_expenditure_price = request.POST["expenditure_price"]
+        input_name = Expenditure.objects.create(expenditure_text = user_expenditure, expenditure_price = user_expenditure_price, user = request.user)
+        return redirect("Calculator:calculator")
+
