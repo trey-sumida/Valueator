068c43f0b8889dfb07b678a6a0b5851a73fd07d2
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-06-05 21:19:06 -1000

    Created general app

--- Valueator-project/Calculator/urls.py
+++ Valueator-project/Calculator/urls.py
@@ -1,9 +1,7 @@
-  
 from django.urls import path
 from . import views
 
 app_name = 'Calculator'
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('calculator/', views.calculator, name='calculator'),
+    path('', views.calculator, name='calculator'),
 ]
--- Valueator-project/Calculator/views.py
+++ Valueator-project/Calculator/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
-
-def homepage(request):
-    return render(request, "Calculator/homepage.html")
-
 def calculator(request):
     return render(request, "Calculator/calculator.html")
