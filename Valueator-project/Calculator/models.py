872c1f03f5e40c075195425629cbb955ad6b213f
ken-10 <46728332+ken-10@users.noreply.github.com>
2020-06-05 21:28:16 -1000

    Created income model

--- Valueator-project/Calculator/models.py
+++ Valueator-project/Calculator/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Income(models.Model):
+    income_text = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(1000000)])
+
+    def __str__(self):
+        return self.income_text
