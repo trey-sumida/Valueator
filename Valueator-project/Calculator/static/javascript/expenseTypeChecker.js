a1c084bd24a5c1f101d3d3d0ea9ee0bc325b36f9
ken-10 <46728332+ken-10@users.noreply.github.com>
2020-06-06 22:47:40 -1000

    If users add the same thing, it auto updates the price

--- Valueator-project/Calculator/static/javascript/expenseTypeChecker.js
+++ Valueator-project/Calculator/static/javascript/expenseTypeChecker.js
@@ -1,8 +1,5 @@
-console.log("dog");
-
 //function hides/shows subscription hours based upon exp-type value
 function check_field_value(new_val) {
-    console.log(new_val);
     if(new_val != 'Digital Subscription') {
         $('#add-hours').hide();
     } else {
@@ -12,12 +9,10 @@ function check_field_value(new_val) {
 
 // this is executed once when the page loads
 $(document).ready(function() {
-    console.log("cat");
+    $("#add-hours").hide();
     // set things up so my function will be called when exp-type changes
     $('#exp-type').change( function() {
-        console.log("cat2");
         var val = document.getElementById('topic_text').value
-        console.log(val);
         check_field_value(val);
     });
 
