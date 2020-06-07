63c7a022f0e33e1fa48977a487558e0ab9b0e613
ken-10 <46728332+ken-10@users.noreply.github.com>
2020-06-06 20:06:56 -1000

    Added topic model

--- Valueator-project/Calculator/admin.py
+++ Valueator-project/Calculator/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Income, Expenditure
+from .models import Income, Expenditure, Topics
 
 admin.site.register(Income)
+admin.site.register(Topics)
 admin.site.register(Expenditure)
